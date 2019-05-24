74bd6e75f9f0b8968b236ec746d0a9e81a24ead0
15827004117 <15827004117@qqc.om>
2019-05-24 21:55:09 +0800

    第一次构建项目

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -0,0 +1,9 @@
+package com.leyou;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class, args);
+    }
+}
