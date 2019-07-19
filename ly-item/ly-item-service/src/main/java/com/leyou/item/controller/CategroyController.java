fd6f188dd92f980802964e9158e2c5b8441c6879
lj <4852247@qq.com>
2019-07-19 16:56:57 +0800

    第五次提交，完成商品分类后台代码编写

--- ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategroyController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategroyController.java
@@ -3,25 +3,29 @@ package com.leyou.item.controller;
 import com.leyou.item.pojo.Category;
 import com.leyou.item.service.CategroyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("categroy")
+@RestController
+@RequestMapping("category")
 public class CategroyController {
 
     @Autowired
     private CategroyService categroyService;
 
     @GetMapping("list")
-    public ResponseEntity<List<Category>> queryCategroyListByParentId(
-            @RequestParam(value = "pid", defaultValue = "0") Long pid) {
-        return null;
-
+    public ResponseEntity<List<Category>> queryCategroyListByParentId(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        List<Category> list = categroyService.queryCategoryListByParentId(pid);
+        if(list == null){
+            // 无数据,返回404状态码
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+            return ResponseEntity.ok(list);
     }
 }
